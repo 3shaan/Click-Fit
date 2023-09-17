dc5a79f4832d408f666770aea33bfc1bea3eae22
Eshan Ahammed <3shan.bd@gmail.com>
2023-09-17 13:58:12 +0600

    some css fix

--- public/JS/data.js
+++ public/JS/data.js
@@ -3,19 +3,19 @@ const services = [
         id:1,
         image:"./Assets/exercise1.jpg",
         title:"Dumbbell Romanian Deadlift",
-        desc:"Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+        desc:"The dumbbell Romanian deadlift is effective at training the glutes and hamstrings at a long muscle length."
     },
     {
         id:2,
         image:"./Assets/exercise2.jpg",
         title:"barbell Front Squat",
-        desc:"Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+        desc:"The front squat is an excellent way to improve our core strength because of demands placed on the erector spinae muscles to maintain the high chest position. "
     },
     {
         id:3,
         image:"./Assets/vollyball.jpg",
-        title:"High Jamb Volley Ball",
-        desc:"Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+        title:"Vertical Jamb Volley Ball",
+        desc:" This improved cardiovascular health, metabolism, bone density, strength, muscle tone, balance and coordination."
     }
 ]
 
@@ -24,25 +24,25 @@ const aboutUs = [
         id:1,
         icon:"fa-solid fa-eye",
         title:"Our Vision",
-        desc:"Lorem ipsum dolor sit amet, consectetur adipisicing elit. Necessitatibus blanditiis sed nulla nesciunt nam ipsa animi, odio quidem incidunt facere."
+        desc:"Our vision is to build a thriving and inclusive fitness community that empowers individuals to achieve their health and wellness goals while fostering a sense of belonging and support."
     },
     {
         id:2,
         icon:"fa-solid fa-rocket",
         title:"Misson",
-        desc:"Lorem ipsum dolor sit amet, consectetur adipisicing elit. Necessitatibus blanditiis sed nulla nesciunt nam ipsa animi, odio quidem incidunt facere."
+        desc:"Our mission is to provide accessible fitness resources, education, and a supportive environment that enables members to improve their physical and mental well-being, build lasting friendships, and positively impact their lives."
     },
     {
         id:3,
         icon:"fa-solid fa-volume-high",
-        title:"Our Vision",
-        desc:"Lorem ipsum dolor sit amet, consectetur adipisicing elit. Necessitatibus blanditiis sed nulla nesciunt nam ipsa animi, odio quidem incidunt facere."
+        title:"Our Motto",
+        desc:"Stronger Together, Every Rep Counts!In every workout, we find purpose and progress, knowing that every rep brings us closer to our best selves.Unite, inspire, and sweat together, because our strength lies in our unity."
     },
     {
         id:4,
         icon:"fa-solid fa-bullseye",
         title:"Target",
-        desc:"Lorem ipsum dolor sit amet, consectetur adipisicing elit. Necessitatibus blanditiis sed nulla nesciunt nam ipsa animi, odio quidem incidunt facere."
+        desc:"Our club welcomes individuals of all ages, backgrounds, and fitness levels who are passionate about improving their health and embracing an active lifestyle. We aim to be a go-to destination for those seeking fitness, wellness, and community."
     }
     
 ]
